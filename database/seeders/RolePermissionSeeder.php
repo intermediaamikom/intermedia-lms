1bf2d1dd103cbb4160482b4687486e18ab8ed697
Didi Prasetyo <didiprasetyo360@gmail.com>
2024-02-25 11:39:03 +0700

    feat: seeding roles: superadmin, admin, and member

---
+++ database/seeders/RolePermissionSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RolePermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Role::create(['name' => 'superadmin']);
+
+        Role::create(['name' => 'admin']);
+
+        Role::create(['name' => 'member']);
+    }
+}
