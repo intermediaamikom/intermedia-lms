0f9b4cba88c36614ad8bb487234fca397c56f2ad
Didi Prasetyo <didiprasetyo360@gmail.com>
2024-02-25 11:39:28 +0700

    feat: seeding some users

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $superadmin = User::factory()->create([
+            'email' => 'superadmin@gmail.com',
+        ]);
+        $superadmin->assignRole('superadmin');
+
+        $admin = User::factory()->create([
+            'email' => 'admin@gmail.com',
+        ]);
+        $admin->assignRole('admin');
+
+        $member1 = User::factory()->create([
+            'email' => 'member1@gmail.com',
+            'division_id' => '05ca9b6e-c6d3-4c3b-bf66-861d89e4137a',
+        ]);
+        $member1->assignRole('member');
+    }
+}
