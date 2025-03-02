e4ad99a1df8fa7955e2f75eb4ff5d0640b5efc6b
iwanharyatno <iwnharry61@gmail.com>
2025-03-02 15:31:31 +0700

    handle signature mismatch during file upload

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 use App\Models\User;
 use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Routing\UrlGenerator;
+use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
@@ -31,5 +34,32 @@ class AppServiceProvider extends ServiceProvider
         if (config('app.env') === 'production') {
             URL::forceScheme('https');
         }
+
+        UrlGenerator::macro(
+            'alternateHasCorrectSignature',
+            function (Request $request, $absolute = true, array $ignoreQuery = []) {
+                $ignoreQuery[] = 'signature';
+
+                $absoluteUrl = url($request->path());
+                $url = $absolute ? $absoluteUrl : '/' . $request->path();
+
+                $queryString = collect(explode('&', (string) $request
+                    ->server->get('QUERY_STRING')))
+                    ->reject(fn($parameter) => in_array(Str::before($parameter, '='), $ignoreQuery))
+                    ->join('&');
+
+                $original = rtrim($url . '?' . $queryString, '?');
+                $signature = hash_hmac('sha256', $original, call_user_func($this->keyResolver));
+                return hash_equals($signature, (string) $request->query('signature', ''));
+            }
+        );
+        UrlGenerator::macro('alternateHasValidSignature', function (Request
+        $request, $absolute = true, array $ignoreQuery = []) {
+            return URL::alternateHasCorrectSignature($request, $absolute, $ignoreQuery)
+                && URL::signatureHasNotExpired($request);
+        });
+        Request::macro('hasValidSignature', function ($absolute = true, array $ignoreQuery = []) {
+            return URL::alternateHasValidSignature($this, $absolute, $ignoreQuery);
+        });
     }
 }
