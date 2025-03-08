4233974b4695917d2360edbb812d14535f4f2e19
Jimly Assidqi <jimlyasidqi692@gmail.com>
2025-03-08 16:04:49 +0700

    Update point keaktifan

--- app/Filament/Resources/FileResource/Pages/EditFile.php
+++ app/Filament/Resources/FileResource/Pages/EditFile.php
@@ -3,8 +3,11 @@
 namespace App\Filament\Resources\FileResource\Pages;
 
 use App\Filament\Resources\FileResource;
+use App\Models\File;
+use App\Models\User;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Database\Eloquent\Model;
 
 class EditFile extends EditRecord
 {
@@ -21,4 +24,16 @@ class EditFile extends EditRecord
     {
         return $this->getResource()::getUrl('index');
     }
+ 
+    protected function handleRecordUpdate(Model $record, array $data): File
+    {
+        $record->update($data);
+        $user = User::find($record->user_id);
+        $user->total_point = $user->total_point + $record->points;
+        $user->save();
+    
+        return $record;
+    }
+
+
 }
